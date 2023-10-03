47beefc5437ad47112b72419f69465b5687e260e
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-10-03 14:23:14 -0700

    New variables.

---
+++ Skripting Examples/Assets/Scripts/Variables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Public Global/Memeber Variables
+    public int Score = 0; // declare a whole number
+    public float ItemAmount = 19.95f; // decimal
+    public string PlayerName = "John Doe"; //Declare a text
+    public bool IsPlayerActivw = true; //declare a true/false
+
+    //Private Global/Member Variables
+    private int _collectableAmount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
