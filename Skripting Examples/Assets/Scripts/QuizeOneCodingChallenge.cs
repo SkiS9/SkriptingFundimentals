a43ead7998446865de364f47c681637944bd914c
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-10-09 14:18:01 -0700

    Scripting for score

---
+++ Skripting Examples/Assets/Scripts/QuizeOneCodingChallenge.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int _score = 0;
+    public string _playerName = "TomBrady";
+    public int _health = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
